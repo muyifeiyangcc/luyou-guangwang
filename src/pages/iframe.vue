12f54a307bb5acda1cba21cca3d55a4ded94007c
杨怡 <639293382@qq.com>
2023-12-11 16:35:58 +0800

    fix:new
    aa

---
+++ src/pages/iframe.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+function aa() {
+  window.postMessage('Hello from parent window!', 'https://pre.xxchat.chat')
+}
+</script>
+
+<template>
+  <div>
+    <div h-100 w-100 bg-black c-white @click="aa">
+      dian
+    </div>
+  </div>
+</template>
